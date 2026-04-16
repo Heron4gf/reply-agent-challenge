d8aba2bccc28f0a8adaa61787818f29ec9d065ab
Heron4gf <herongiambuzzi@gmail.com>
2026-04-16 18:34:19 +0200

    updated main

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
+import os
 from dotenv import load_dotenv
-from models.input_data import UserProfile
 from utils.call_llm import call_llm
 from models.response_id import FraudUserTransactions
+from typing import List
+from utils.read_prompt import read_file
 
 load_dotenv()
 
-def getUsers() -> list[UserProfile]:
-    return users = load_users(dataset_path / "users.json")
+def getUsers(path: str = "output/enriched_users") -> List[str]:
+    if not os.path.exists(path):
+        return []
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.json')]
 
-def process_solution(dataset: str, solution_path: str = "result.txt"):
+def getUser(path: str) -> str:
+    return read_file(path)
+
+def process_solution(solution_path: str = "result.txt"):
     with open(solution_path, "w", encoding="utf-8") as file:
-        for user in getUsers():
+        for user_path in getUsers():
+            user_data = getUser(user_path)
+            
             suspect_transactions = call_llm(
                 prompt_id="fraud_detection",
-                input=user.model_dump_json(),
+                input=user_data,
                 output_format=FraudUserTransactions
             )
             
-            for fraud in suspect_transactions.fraudolent_transactions:
-                file.write(f"{fraud.transaction_id}\n")
+            if suspect_transactions and suspect_transactions.fraudolent_transactions:
+                for fraud in suspect_transactions.fraudolent_transactions:
+                    file.write(f"{fraud.transaction_id}\n")
+
+if __name__ == "__main__":
+    process_solution()
